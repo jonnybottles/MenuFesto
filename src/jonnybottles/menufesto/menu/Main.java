f4940582b75a6935ee95d76a92737803ff1d7142
jbutler <xxbutler86xx@gmail.com>
2024-01-12 19:37:03 -0500

    Added manifes file

--- src/jonnybottles/menufesto/menu/Main.java
+++ src/jonnybottles/menufesto/menu/Main.java
@@ -2,7 +2,8 @@ package jonnybottles.menufesto.menu;
 
 public class Main {
     public static void main(String[] args) {
-//        Menu theMainMenu = new Menu("Menu Festo", "Main Menu");
+        Menu theMainMenu = new Menu("Menu Festo", "Main Menu");
+        theMainMenu.start();
 
     }
 }
